d1bd560ad24d8da82248c9d2a65820f5f616e6e0
Max Mathys <max.mathys@gmail.com>
2022-06-14 14:36:23 +0000

    support internal and external functions

--- hybrid_test.py
+++ hybrid_test.py
@@ -7,19 +7,25 @@ def foo(data):
     print(f"passed data: {data}")
     return 0
 
-def call_faasm_function(fn, arg):
-    source = inspect.getsource(fn)
+def call_faasm_function(fn, arg=None, entry=None):
+    if callable(fn):
+        entry = fn.__name__
+        source = inspect.getsource(fn)
 
-    m = hashlib.sha256()
-    m.update(source.encode())
-    func_hash = m.hexdigest()[:32]
+        m = hashlib.sha256()
+        m.update(source.encode())
+        func_name = m.hexdigest()[:32]
 
-    # upload function
-    host, port = get_faasm_upload_host_port()
-    url = "http://{}:{}/p/{}/{}".format(host, port, "python", func_hash)
-    response = requests.put(url, data=source)
-    print("Upload response ({}): {}".format(response.status_code, response.text))
-    
+        # upload function
+        host, port = get_faasm_upload_host_port()
+        url = "http://{}:{}/p/{}/{}".format(host, port, "python", func_name)
+        response = requests.put(url, data=source)
+        print("Upload response ({}): {}".format(response.status_code, response.text))
+    elif type(fn) is str:
+        func_name = fn
+    else:
+        raise Exception("function must be function or string")
+
     # invoke function
     host, port = get_faasm_invoke_host_port()
     url = "http://{}:{}".format(host, port)
@@ -27,17 +33,25 @@ def call_faasm_function(fn, arg):
         "user": "python",
         "function": "py_func",
         "py_user": "python",
-        "py_func": func_hash,
+        "py_func": func_name,
         "python": True,
-        "py_entry": fn.__name__,
-        "input_data": arg
     }
+
+    if entry:
+        data["py_entry"] = entry
+
+    if arg:
+        data["input_data"] = arg
 
     response = requests.post(url, json=data)
     print("Invoke response ({}): {}".format(response.status_code, response.text))
 
 def main():
+    # internal call
     call_faasm_function(foo, "bar")
+
+    # external call
+    call_faasm_function("hello") 
 
 if __name__ == "__main__":
     main()
